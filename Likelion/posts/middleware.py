00e3c6044b33e262549ee5eff46338bba9f4516b
luckylee524 <dbleecomkr@naver.com>
2025-04-12 12:31:06 +0900

    modify file

--- Likelion/posts/middleware.py
+++ Likelion/posts/middleware.py
@@ -8,6 +8,8 @@ class RequestLoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # 요청 URL을 로그에 포함시킴
         logger.info(f"[{datetime.now()}] {request.method} {request.get_full_path()}")
+        
         response = self.get_response(request)
         return response
